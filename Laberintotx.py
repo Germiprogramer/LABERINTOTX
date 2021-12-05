9c67974f6787f1d1cea73b6dcd3670f7ccdbe837
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-05 20:02:45 +0100

    COMMIT

--- Laberintotx.py
+++ Laberintotx.py
@@ -6,41 +6,37 @@ laberinto = [
     ['X', 'X', 'X', 'X', 'S']
     ]
 
-muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
-
-muro = "X"
-
-i = 0
-j = 0
-k = 0
-
-def tablero(laberinto):
-    muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
-#cuando ponemos for i in range (12), nos referimos a los 12 primeros elementos de MURO????
-    for i in range (12):
-        laberinto[muro[i][0]][muro[i][1]]
-    for i in range (5):
-        print[laberinto[i]]
+x = 0
+y = 0
 
 
+print("Empiezas en la posición (0,0)")
+print((laberinto[y])[x])
 
+while (laberinto[y])[x] == ' ':
+    direccion = input("Selecciona a la dirección a la cual quieres ir: ARRIBA, ABAJO, IZQUIERDA, DERECHA  >>>>  ")
+    
+    if direccion == "ABAJO":
+        x = x
+        y = y - 1
+    elif direccion == "ARRIBA":
+        x = x
+        y = y + 1
+    elif direccion == "IZQUIERDA":
+        x = x - 1
+        y = y
+    elif direccion == "DERECHA":
+        x = x
+        y = y + 1
+    else:
+        print("elige bien")
+    
+    print((laberinto[y])[x])
 
-def mi_metodo(self, nombre):
-    print("{}.mi_metodo({}".format(self, nombre))
-
-def __init__(self, x, y):
-    self.x = x
-    self.y = y
-
-def mostrar(self):
-    print("Punto ({}, {})".format(self.x, self.y))
-
-
-tablero(laberinto)
-
-#DUDA: COMO HAGO PARA QUE ME SAQUE UNA SOLA CASILLA, Y NO LA FILA COMPLETA
-
-
-#Punto = (x, y)
-#p = Punto(1, 2)
-#p.mostrar()
+    print("Estás en la casilla ({},{})".format(x,y))
+    if (laberinto[y])[x] == ' ':
+        print("La casilla elegida NO es un muro, puede continuar en el laberinto")
+    else:
+        print("La casilla es un muro")
+    
+    print(direccion)
--- pruebas.py
+++ pruebas.py
@@ -6,24 +6,79 @@ laberinto = [
     ['X', 'X', 'X', 'X', 'S']
     ]
 
-muro = (0,1)
-
 casillasmalas = ((laberinto[0])[1] , (laberinto[0])[2], (laberinto[0])[3], (laberinto[0])[4], (laberinto[1])[1], (laberinto[2])[1], (laberinto[2])[3], (laberinto[3])[3], (laberinto[4])[0], (laberinto[4])[1], (laberinto[4])[2], (laberinto[4])[3])
 
+print((laberinto[0])[4])
 
 print(casillasmalas)
 
-direccion = input("Selecciona a la dirección a la cual quieres ir: ARRIBA, ABAJO, IZQUIERDA, DERECHA")
+x = 0
+y = 0
 
-if direccion == "ARRIBA":
-    x = x - 1
-    y = y
-elif direccion == "ABAJO":
-    x = x + 1
-    y = y
-elif direccion == "IZQUIERDA":
-    x = x
-    y = y + 1
-elif direccion == "DERECHA":
-    x = x
-    y = y - 1
+
+print("Empiezas en la posición (0,0)")
+print((laberinto[y])[x])
+
+while (laberinto[y])[x] == ' ':
+    direccion = input("Selecciona a la dirección a la cual quieres ir: ARRIBA, ABAJO, IZQUIERDA, DERECHA  >>>>  ")
+    
+    if direccion == "ABAJO":
+        x = x
+        y = y - 1
+    elif direccion == "ARRIBA":
+        x = x
+        y = y + 1
+    elif direccion == "IZQUIERDA":
+        x = x - 1
+        y = y
+    elif direccion == "DERECHA":
+        x = x
+        y = y + 1
+    else:
+        print("elige bien")
+    
+    print((laberinto[y])[x])
+
+    print("Estás en la casilla ({},{})".format(x,y))
+    if (laberinto[y])[x] == ' ':
+        print("La casilla elegida NO es un muro, puede continuar en el laberinto")
+    else:
+        print("La casilla es un muro")
+    
+    print(direccion)
+
+muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
+
+muro = "X"
+
+
+def tablero(laberinto):
+    muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
+#cuando ponemos for i in range (12), nos referimos a los 12 primeros elementos de MURO????
+
+
+print((laberinto[1])[1])
+
+coordenadaslaberinto = (laberinto[y])[x]
+casillasmalas = (laberinto[{}])[{}].format(muro)
+
+def mi_metodo(self, nombre):
+    print("{}.mi_metodo({}".format(self, nombre))
+
+def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+def mostrar(self):
+    print("Punto ({}, {})".format(self.x, self.y))
+
+
+tablero(laberinto)
+
+#DUDA: COMO HAGO PARA QUE ME SAQUE UNA SOLA CASILLA, Y NO LA FILA COMPLETA
+
+
+#Punto = (x, y)
+#p = Punto(1, 2)
+#p.mostrar()
+    
